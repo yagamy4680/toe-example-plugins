7db79215151020f1c49b1164300600997add25f3
yagamy4680 <yagamy@gmail.com>
2018-10-12 03:13:00 +0800

    ps-demo2: initial commit

---
+++ plugins/ps-demo2/src/monitors/memory.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var BaseMonitor = require('./base');
+
+class MemoryMonitor extends BaseMonitor {
+    constructor(interval) {
+        super(interval);
+        this.name = 'memory';
+    }
+
+    capture() {
+        /**
+         * The memoryUsage() shall output an object:
+         * 
+         *  { rss: 32477184, heapTotal: 18169856, heapUsed: 8554328, external: 36657 }
+         */
+        var usage = process.memoryUsage();
+        this.emit('data-updated', 'memory', '0', usage);
+    }
+}
+
+module.exports = exports = MemoryMonitor;
